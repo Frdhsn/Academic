1fa5d44ec3a555332acdbc19553c5db38e6e40ac
Frdhsn <iamforu299@gmail.com>
2020-01-08 23:08:24 +0600

    Create SJF(Pre-emptive).cpp

---
+++ 3-1/OS Lab/SJF(Pre-emptive).cpp
@@ -0,0 +1,132 @@
+// SJF(Pre-emptive)
+/*
+TAT - Turn Around Time
+WT  - Waiting Time
+AT  - Arrival Time
+BT  - Burst Time
+CT  - Completion Time
+
+Formula:
+
+Turn Around Time = Completion Time  – Arrival Time
+Waiting Time     = Turn Around Time – Burst Time
+
+*/
+#include<bits/stdc++.h>
+using namespace std;
+#define ff first
+#define ss second
+typedef pair<int,int> pii;
+
+struct process{
+    string name;
+    int AT , BT ,CT, priority , TAT , WT , ipos;
+    process(){
+        name="";
+        AT=0;
+        BT=0;
+        priority=0;
+        TAT=0;
+        WT=0;
+    }
+};
+void printAverage_WT(process Array[],int N){
+    double sum=0.0 , avg=0.0;
+
+    for(int i=1;i<=N;i++){
+            sum+=Array[i].WT;
+    }
+    avg = sum/(N*1.0);
+    cout<<"\nAverage waiting time = "<<fixed<<setprecision(10)<<avg<<endl;
+}
+void printAverage_TAT(process Array[],int N){
+    double sum=0.0 , avg=0.0;
+
+    for(int i=1;i<=N;i++){
+            sum+=Array[i].TAT;
+    }
+    avg = sum/(N*1.0);
+    cout<<"\nAverage TurnAround time = "<<fixed<<setprecision(10)<<avg<<endl;
+}
+void printWT(process Array[],int N){
+    cout<<"\nProcess----Waiting Time\n";
+
+    for(int i=1;i<=N;i++){
+            cout<<"  "<<Array[i].name<<"\t\t"<<Array[i].WT<<endl;
+    }
+}
+void printTAT(process Array[],int N){
+    cout<<"\nProcess----TurnAround Time\n";
+    for(int i=1;i<=N;i++){
+            cout<<"  "<<Array[i].name<<"\t\t"<<Array[i].TAT<<endl;
+    }
+}
+int main (){
+    int N;
+    cout<<"Enter the number of processes: ";
+    cin >> N;
+
+    process Array[N+5];
+    bool taken[N+5];
+    memset(taken,0,sizeof taken);
+
+    priority_queue<pii,vector<pii>,greater<pii>>PQ;
+
+
+    for(int i=1;i<=N;i++){
+            cout<<"Enter name , Arrival time ,burst time : ";
+            cin >> Array[i].name >> Array[i].AT >> Array[i].BT;
+            // if arrival time is not given then use this
+            // cin >> Array[i].name >> Array[i].BT;
+            // and add comment to previous line
+    }
+    int time = 0 , done = 0;
+
+    while(1){
+        for(int i=1;i<=N;i++){
+            if(taken[i]==0){
+
+                if(Array[i].AT<=time){
+                    PQ.push({Array[i].BT , i});
+                    taken[i]=1;
+                }
+            }
+        }
+
+        pii u = PQ.top();
+        PQ.pop();
+        time++;
+        if(u.ff==1){
+                int id = u.ss;
+                Array[id].CT = time;
+                Array[id].TAT = Array[id].CT - Array[id].AT;
+                Array[id].WT = Array[id].TAT - Array[id].BT;
+                done++;
+        }
+        else{
+            u.ff--;
+            PQ.push({u.ff , u.ss});
+        }
+        if(done==N)break;
+
+    }
+
+    printWT(Array,N);
+    printAverage_WT(Array,N);
+
+    printTAT(Array,N);
+    printAverage_TAT(Array,N);
+    return 0;
+}
+/*
+number of process
+process_name arrival_time burst_time
+
+4
+P1 0 8
+P2 1 4
+P3 2 9
+P4 3 5
+
+*/
+
